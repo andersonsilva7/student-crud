df0323926b7de929f09972e32f438ee0bcfafedd
andersonsilva7 <andersonsilva7.ti@gmail.com>
2024-06-23 20:47:52 -0300

    Configurar roteamento para a aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+// src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StudentListComponent } from './studentComponents/student-list/student-list.component';
+import { StudentFormComponent } from './studentComponents/student-form/student-form.component';
+import { StudentDetailsComponent } from './studentComponents/student-details/student-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'students', component: StudentListComponent },
+  { path: 'students/add', component: StudentFormComponent },
+  { path: 'students/edit/:id', component: StudentFormComponent },
+  { path: 'students/details/:id', component: StudentDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
